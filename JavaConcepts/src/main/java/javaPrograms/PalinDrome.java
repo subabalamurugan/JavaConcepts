8a27d3c33fa6916b973c3b21257cd09eee4ae33c
subabalamurugan <83529421+subabalamurugan@users.noreply.github.com>
2021-05-07 00:39:32 +0530

    Basics Java Concepts

--- JavaConcepts/src/main/java/javaPrograms/PalinDrome.java
+++ JavaConcepts/src/main/java/javaPrograms/PalinDrome.java
@@ -8,19 +8,20 @@ public class PalinDrome {
 
 	public static void main(String[] args) 
 	{
-		String a ="madam", rev="";
-		char palin[]=a.toCharArray();
-		System.out.println("Input : madam");
+		String a , rev="";
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the string : ");
+		a = sc.nextLine();
+		char palin[]=a.toCharArray();		
 			for(int i=palin.length-1;i>=0;i--)
 			{
-				rev=rev + a.charAt(i);		
-			    
+				rev=rev + a.charAt(i);				    
 			}
 			
 	       if(a.equals(rev))
 				System.out.println("Given String is a Palindrome");
 			else
-				System.out.println("Given String is a not palindrome");  
+				System.out.println("Given String is a not palindrome"); 
 			
 	}
 
